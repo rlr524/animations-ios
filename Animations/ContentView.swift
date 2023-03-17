76ce2bdb096232a3dd8968d6407d16af6b8c0b15
Rob Ranf <rob@emiyaconsulting.com>
2023-03-17 12:56:42 -0700

    Animation tests

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,12 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 1.0
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            Spacer()
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+            .blur(radius: (animationAmount - 1) * 3)
+            .animation(.default, value: animationAmount)
+            Spacer()
         }
         .padding()
     }
