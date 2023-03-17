8884148a232b9edc9f82124f694e7c11245f33b5
Rob Ranf <rob@emiyaconsulting.com>
2023-03-17 11:35:59 -0700

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Rob Ranf on 3/17/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
